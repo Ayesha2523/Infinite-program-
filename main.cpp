d9c60c6ff2de1ff9e690c7756560fbd9d0499d0d
Ayesha2523 <111120173+Ayesha2523@users.noreply.github.com>
2023-07-05 12:56:16 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    
+   
+
+    string users[]={"1","3","5","7",};
+    
+    cout<<users[0];
+    
+    
+    int i;
+    
+    while(i < 3) {
+  cout << i << "\n";
+  
+    }
+  
+
+switch (i) {
+  case 1:
+    cout << "print: 3 found";
+    break;
+  case 2:
+    cout << "print: 8 not found";
+    break;
+
+
+ 
+}  
+
+}
